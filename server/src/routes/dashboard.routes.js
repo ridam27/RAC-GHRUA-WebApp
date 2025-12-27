ef36a297c61b49e03b2bd4f8bf5fc0b0a825fed4
Ridam Vitthal Satkar <ridamsatkar1@gmail.com>
2025-12-27 23:57:03 +0530

    backend for dashboards updated

--- server/src/routes/dashboard.routes.js
+++ server/src/routes/dashboard.routes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const { verifyToken } = require("../middlewares/auth.middleware");
-const { memberDashboard } = require("../controllers/dashboard.controller");
+
+const {
+  memberDashboard,
+  asstAdminDashboard,
+  adminDashboard
+} = require("../controllers/dashboard.controller");
 
 router.get("/member", verifyToken(), memberDashboard);
+router.get("/asst-admin", verifyToken(["ASST_ADMIN", "ADMIN"]), asstAdminDashboard);
+router.get("/admin", verifyToken(["ADMIN"]), adminDashboard);
 
 module.exports = router;
